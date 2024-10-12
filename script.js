c524b7e76c8968eadbcf6d187787cf53efde94bb
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-12 19:35:36 +1000

    Create getComputerChoice function
    this generates a random number between 1 & 3 and assigns rock, paper, or
    scissors depending on the number, then returns it.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice() {
+  // Generates a random number between 1 and 3
+  let n = Math.floor(Math.random() * 3 + 1);
+  
+  // Return computer choice based on random number
+  if (n === 1) {
+    return "Rock";
+  } else if (n === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+// Assign computers choice 
+let computerChoice = getComputerChoice();
