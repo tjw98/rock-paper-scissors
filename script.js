3ff9448b2ad0c582e0cb759b46d939caf3fc891b
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-12 21:22:13 +1000

    Minor changes across program

--- script.js
+++ script.js
@@ -20,40 +20,46 @@ function getHumanChoice() {
 }
 
 // Decide winner of round
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice, roundNumber) {
+  console.log(`Round ${roundNumber}:`);
   // Win condition
   if (humanChoice === "Rock" && computerChoice === "Scissors"
     || humanChoice === "Scissors" && computerChoice === "Paper"
     || humanChoice === "Paper" && computerChoice === "Rock"
   ) {
-    console.log("You win!");
-    return humanScore += 1;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore += 1;
   // Lose condition
   } else if (humanChoice === "Rock" && computerChoice === "Paper"
     || humanChoice === "Scissors" && computerChoice === "Rock"
     || humanChoice === "Paper" && computerChoice === "Scissors"
   ) {
-    console.log("You lose.");
-    return computerScore += 1;
+    console.log(`Computer wins. ${computerChoice} beats ${humanChoice}`);
+    computerScore += 1;
   // Tie condition
   } else {
     console.log("It's a tie.");
   }
 }
 
+function playGame() {
+  for(let i = 0; i < 5; i++){
+    playRound(getHumanChoice(), getComputerChoice(), roundNumber);
+    roundNumber += 1;
+  }
+
+  if (humanScore > computerScore) {
+    console.log(`You win the game ${humanScore} to ${computerScore}`);
+  } else if (computerScore > humanScore) {
+    console.log(`You lose the game ${humanScore} to ${computerScore}`);
+  } else {
+    console.log("The game is a tie, what are the odds.");
+  }
+}
+
 // Initialise score variables
 let computerScore = 0;
 let humanScore = 0;
+let roundNumber = 1;
 
-// Assign computers choice 
-let computerChoice = getComputerChoice();
-
-// Assign humans choice
-let humanChoice = getHumanChoice();
-
-// Print human and computer choices to console
-console.log(humanChoice);
-console.log(computerChoice);
-
-// Play round of game
-playRound(humanChoice, computerChoice);
+playGame();
