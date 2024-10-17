70f1728ffb7784e20a068f4c87a5cc464347705c
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-17 20:13:08 +1000

    Make game playable outside of console

--- script.js
+++ script.js
@@ -12,50 +12,74 @@ function getComputerChoice() {
   }
 }
 
+function updateScoreBoard() {
+  scoreBoard.textContent = `Round: ${roundNum} Player Score: ${humanScore} Computer Score: ${computerScore}`;
+}
+
 // Decide winner of round
 function playRound(humanChoice, computerChoice) {
+  roundNum += 1;
   // Win condition
   if (humanChoice === "Rock" && computerChoice === "Scissors"
     || humanChoice === "Scissors" && computerChoice === "Paper"
     || humanChoice === "Paper" && computerChoice === "Rock"
   ) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    gameResults.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
     humanScore += 1;
   // Lose condition
   } else if (humanChoice === "Rock" && computerChoice === "Paper"
     || humanChoice === "Scissors" && computerChoice === "Rock"
     || humanChoice === "Paper" && computerChoice === "Scissors"
   ) {
-    console.log(`Computer wins. ${computerChoice} beats ${humanChoice}`);
+    gameResults.textContent = `Computer wins. ${computerChoice} beats ${humanChoice}`;
     computerScore += 1;
   // Tie condition
   } else {
-    console.log("It's a tie.");
+    gameResults.textContent = "It's a tie.";
   }
 }
 
 // Initialise score variables
 let computerScore = 0;
 let humanScore = 0;
+let roundNum = 1;
 
-// Create game container
-const gameContainer = document.createElement("div");
-document.body.appendChild(gameContainer);
+const gameContainer = document.querySelector(".game-container");
+const buttonContainer = document.querySelector(".button-container");
+const scoreBoard = document.querySelector(".scoreboard");
+const gameResults = document.createElement("p");
+gameResults.textContent = "Choose your weapon! First to five wins!";
+gameContainer.appendChild(gameResults);
+scoreBoard.textContent = `Round: ${roundNum} Player Score: ${humanScore} Computer Score: ${computerScore}`;
 
 // Create rps buttons
 const rockBtn = document.createElement("button");
 rockBtn.textContent = "Rock";
-gameContainer.appendChild(rockBtn);
+buttonContainer.appendChild(rockBtn);
 
 const paperBtn = document.createElement("button");
 paperBtn.textContent = "Paper";
-gameContainer.appendChild(paperBtn);
+buttonContainer.appendChild(paperBtn);
 
 const scissorsBtn = document.createElement("button");
 scissorsBtn.textContent = "Scissors";
-gameContainer.appendChild(scissorsBtn);
+buttonContainer.appendChild(scissorsBtn);
 
 // Add click event listeners and play game on click
-rockBtn.addEventListener("click", () => playRound("Rock", getComputerChoice()));
-paperBtn.addEventListener("click", () => playRound("Paper", getComputerChoice()));
-scissorsBtn.addEventListener("click", () => playRound("Scissors", getComputerChoice()));
+rockBtn.addEventListener(
+  "click", () => {
+    playRound("Rock", getComputerChoice());
+    updateScoreBoard();
+  });
+  
+paperBtn.addEventListener(
+  "click", () => {
+    playRound("Paper", getComputerChoice());
+    updateScoreBoard();
+  });
+    
+scissorsBtn.addEventListener(
+  "click", () => {
+    playRound("Scissors", getComputerChoice());
+    updateScoreBoard();
+  });  ``
